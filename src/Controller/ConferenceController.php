0cb0df4d405922d630bc81ef96e43da98c9662e9
Adrien Roches <adrien@roc-it.tech>
2026-01-13 15:54:03 +0100

    [sf-start] Update the ConferenceController to save data

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,16 +5,27 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Conference;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference/{name}/{start}/{end}', name: 'app_conference_new')]
-    public function newConference(string $name, string $start, string $end, /* ... */): Response
+    #[Route(
+        path: '/conference/{name}/{start}/{end}',
+        name: 'app_conference_new',
+        requirements: [
+            'name' => Requirement::ASCII_SLUG,
+            'start' => Requirement::DATE_YMD,
+            'end' => Requirement::DATE_YMD,
+        ],
+        methods: ['GET'],
+    )]
+    public function newConference(string $name, string $start, string $end, EntityManagerInterface $entityManager): Response
     {
-        $conference = (new Conference())
+        $conference = new Conference()
             ->setName($name)
             ->setDescription('Some generic description')
             ->setAccessible(true)
@@ -22,7 +33,8 @@ class ConferenceController extends AbstractController
             ->setEndAt(new \DateTimeImmutable($end))
         ;
 
-        //...
+        $entityManager->persist($conference);
+        $entityManager->flush();
 
         return new Response('Conference created');
     }
