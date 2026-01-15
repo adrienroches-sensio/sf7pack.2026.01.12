62cec34bd436d0100429c0efc415a476e5281349
Adrien Roches <adrien@roc-it.tech>
2026-01-15 12:10:29 +0100

    [sf-advanced] Use alias on DatabaseConferenceSearch

--- src/Search/Conference/DatabaseConferenceSearch.php
+++ src/Search/Conference/DatabaseConferenceSearch.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace App\Search\Conference;
 
 use App\Repository\ConferenceRepositoryInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 
+#[AsAlias(ConferenceSearchInterface::class)]
 final class DatabaseConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
