38459b1e470a20f9418a69f9175d910e734462c9
Adrien Roches <adrien@roc-it.tech>
2026-01-15 11:55:08 +0100

    [sf-advanced] Add ApiConferenceSearch class

---
+++ src/Search/Conference/ApiConferenceSearch.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Search\Conference;
+
+final class ApiConferenceSearch implements ConferenceSearchInterface
+{
+    public function searchByName(?string $name = null): array
+    {
+        return [];
+    }
+}
