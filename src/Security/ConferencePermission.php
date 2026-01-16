a7090ee2eefd3efd98da3bef61a87424d8613e80
Adrien Roches <adrien@roc-it.tech>
2026-01-16 14:35:45 +0100

    [sf-advanced] Create voter for Can CREATE conference

--- src/Security/ConferencePermission.php
+++ src/Security/ConferencePermission.php
@@ -7,6 +7,7 @@ namespace App\Security;
 enum ConferencePermission
 {
     public const EDIT = 'conference/edit';
+    public const CREATE = 'conference/create';
 
     public static function belong(string $permission): bool
     {
